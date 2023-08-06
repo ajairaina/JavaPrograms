edc6fd1b5317fbac1bc458ad545924a17602285a
ajairaina <ajaivenkat96@gmail.com>
2023-08-06 23:53:03 +0530

    Data driven commit

---
+++ DataDriven/src/logintestcases/bothcorrect.java
@@ -0,0 +1,24 @@
+package logintestcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class bothcorrect {
+
+	@Test
+	public void loginwithbothcorrect() {
+		
+		System.setProperty("webdriver.chrome.driver",
+				 "C:\\Users\\DELL\\eclipse-workspace\\Projectajai\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		
+		driver.findElement(By.id("txtUsername")).sendKeys("Admin");
+		driver.findElement(By.id("txtPassword")).sendKeys("admin123");
+		driver.findElement(By.id("btnLogin")).click();
+		
+		driver.quit();
+	}
+}
